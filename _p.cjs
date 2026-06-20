23058ab59911cca59c998fd95402c2035eeb923d
bestop <bestopjoe@gmail.com>
2026-06-20 17:26:59 +0800

    1

--- _p.cjs
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const cwd = 'c:\\Users\\shenhq\\Documents\\GitHub\\iList';
-try {
-  execSync('git add -A && git commit -m "fix: improve snapshot image quality with 2x DPI scaling" && git push origin main', { stdio: 'inherit', cwd, timeout: 60000 });
-  console.log('Done!');
-} catch (e) {
-  console.error('Error:', e.message);
-}
