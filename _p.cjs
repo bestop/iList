2f5c71de92e059fe88a4e5d8899e30dc1f543911
bestop <bestopjoe@gmail.com>
2026-06-20 19:53:36 +0800

    chore: remove unused _p.cjs

--- _p.cjs
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const cwd = 'c:\\Users\\shenhq\\Documents\\GitHub\\iList';
-try {
-  execSync('git add -A && git commit -m "feat: admin user management panel - list, toggle role, delete users" && git push origin main', { stdio: 'inherit', cwd, timeout: 60000 });
-  console.log('Done!');
-} catch (e) {
-  console.error('Error:', e.message);
-}
